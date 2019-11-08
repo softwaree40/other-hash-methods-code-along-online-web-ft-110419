1bbc615a3586f3a8b99f08c179362b12982da458
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-08 07:17:41 +0000

    Automatically backed up by Learn

--- lib/min.rb
+++ lib/min.rb
@@ -12,7 +12,7 @@ def get_the_min(groceries)
   #code your solution here!
   groceries.each do |fruit,list|
     if fruit == :vegetable
-       list.each do |items|
+       list.each do {|items| items ==
       binding.pry
         end
      end
